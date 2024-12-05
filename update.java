49bde8488df30418769fd54c140ff7689398a4d6
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-05 16:03:56 +0530

    Add files via upload

---
+++ update.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class update{
+    public static void main(String args[]){
+        int n=5;
+        System.out.println(++n);
+        //post inc
+        System.out.println(n++);
+        //pre dec
+        System.out.println(--n);
+        //post dec
+        System.out.println(n--);
+    }
+}
