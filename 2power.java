81d12ec6c9cfff55c35268cc971c3000b5033ab7
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-05 12:37:44 +0530

    Add files via upload

---
+++ 2power.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class mult{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=1;i<n;i=i*2)
+        {
+            
+                System.out.println(i*2);
+
+        }
+    }
+}
